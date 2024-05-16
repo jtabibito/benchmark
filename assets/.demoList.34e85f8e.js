f4297eb81703611ccb29ccd60459c90ec5d7029e
jtabibito <jtabibito@users.noreply.github.com>
2024-05-16 06:48:04 +0000

    deploy: 05c73fcc7a03bd2675cff85bbfb44eab6e5f937a

---
+++ assets/.demoList.34e85f8e.js
@@ -0,0 +1,1 @@
+const a=[{src:"animation-benchmark",label:"Animation"},{src:"collider-benchmark",label:"Collider"},{src:"material",label:"Material"},{src:"mesh",label:"Mesh"}],e=[{src:"point-light",label:"Point Light"}],l={Benchmark:a,Light:e};export{a as Benchmark,e as Light,l as default};
--- assets/.demoList.e99f51ac.js
+++
@@ -1,1 +0,0 @@
-const a=[{src:"animation-benchmark",label:"Animation"},{src:"collider-benchmark",label:"Collider"},{src:"material",label:"Material"}],l=[{src:"point-light",label:"Point Light"}],t={Benchmark:a,Light:l};export{a as Benchmark,l as Light,t as default};
